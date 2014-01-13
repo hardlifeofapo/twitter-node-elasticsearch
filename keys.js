81e455e639f068e1f9816424a96f28961487ea95
Pablo Casado <p.casado.arias@gmail.com>
2014-01-14 00:42:04 +0100

    Adding placeholder for Twitter keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+var keys = {
+	"consumer_key": "YOUR_CONSUMER_KEY",
+  "consumer_secret": "YOUR_COSUMER_SECRET",
+  "access_token_key": "YOUR_TOKEN_KEY",
+  "access_token_secret": "YOUR_TOKEN_SECRET"
+}
+
+exports.keys	 = keys;
